6f8639c246c2dc98fc416e7c820f87b042cd9044
NRG-Drink <beatdz93@hotmail.com>
2024-06-03 11:20:50 +0200

    chore: start implement form

---
+++ WEE.BlazorWebappTest/WEE.BlazorWebappTest/Components/Models/MyPerson.cs
@@ -0,0 +1,10 @@
+﻿namespace WEE.BlazorWebappTest.Components.Models;
+
+public record MyPerson
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public DateTime BirthDate { get; set; }
+    public bool IsMale { get; set; }
+}
