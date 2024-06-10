4a07a9c056a3484a532f7ccb153be2a68f871ca1
NRG-Drink <beatdz93@hotmail.com>
2024-06-10 12:00:34 +0200

    chore: edited form

--- WEE.BlazorWebappTest/WEE.BlazorWebappTest/Components/Models/MyPerson.cs
+++ WEE.BlazorWebappTest/WEE.BlazorWebappTest/Components/Models/MyPerson.cs
@@ -1,9 +1,16 @@
-﻿namespace WEE.BlazorWebappTest.Components.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WEE.BlazorWebappTest.Components.Models;
 
 public record MyPerson
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    [Required(ErrorMessage = "First name is required.")]
+    [StringLength(10)]
+    public required string FirstName { get; set; }
+    [Required] 
+    public required string LastName { get; set; }
+    [Required]
+    [Range(0, 150)]
     public int Age { get; set; }
     public DateTime BirthDate { get; set; }
     public bool IsMale { get; set; }
