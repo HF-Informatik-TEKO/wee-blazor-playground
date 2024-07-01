5b28de6150f062ca399259a72052d235c1c252a8
NRG-Drink <beatdz93@hotmail.com>
2024-07-01 11:02:51 +0200

    chore: add my todo list example

---
+++ WEE.BlazorWebappTest/WEE.BlazorWebappTest/Components/Pages/MyTodo/MyTodoSorting.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace WEE.BlazorWebappTest.Components.Pages.MyTodo;
+
+public enum MyTodoSorting
+{
+    None,
+    Done,
+    Name,
+    DueDate,
+    Created
+}
